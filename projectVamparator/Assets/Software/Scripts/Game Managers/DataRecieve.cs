817d4dd7d64e835b9837494651e6736cbfc23759
AnyPerson1 <arda45simsek@gmail.com>
2024-09-21 00:16:58 +0300

    last commit edit

--- projectVamparator/Assets/Software/Scripts/Game Managers/DataRecieve.cs
+++ projectVamparator/Assets/Software/Scripts/Game Managers/DataRecieve.cs
@@ -31,7 +31,6 @@ namespace SpaceVandal.Data
                 while (!dataReader.EndOfStream)
                 {
                     string line = dataReader.ReadLine();
-
                     if (line == key)
                     {
                         data = line;
