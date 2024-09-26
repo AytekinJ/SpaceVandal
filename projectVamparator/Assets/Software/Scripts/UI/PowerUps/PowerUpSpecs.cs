32550d8c0b5298cca960606a6f0a933e3ef19349
AnyPerson1 <116653247+AnyPerson1@users.noreply.github.com>
2024-09-26 14:35:26 +0300

    power up specs

---
+++ projectVamparator/Assets/Software/Scripts/UI/PowerUps/PowerUpSpecs.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUpSpecs : MonoBehaviour
+{
+    public int storageBoost;
+    public int healthBoost;
+    public int damageBoost;
+    public int collectSpeedBoost;
+    public int speedBoost;
+    public int collectAmountBoost;
+    public int extraLivesBoost;
+}
