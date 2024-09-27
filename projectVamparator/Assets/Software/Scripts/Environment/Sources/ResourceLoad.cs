3bb15057ae6d0d9af3bc4f345e60f262dc0c605c
AnyPerson1 <116653247+AnyPerson1@users.noreply.github.com>
2024-09-27 14:08:05 +0300

    resource load system

---
+++ projectVamparator/Assets/Software/Scripts/Environment/Sources/ResourceLoad.cs
@@ -0,0 +1,21 @@
+﻿using SpaceVandal.Data;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResourceLoad : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            DataSend ds = new DataSend();
+            DataRecieve dr = new DataRecieve();
+            int dataToGo = collision.gameObject.GetComponent<PlayerStorageManager>().CurrentStorage;
+            ds.resourceDataChange("Wood", "Addition", dataToGo);
+            collision.gameObject.GetComponent<PlayerStorageManager>().CurrentStorage = 0;
+            Debug.Log("Kaynaklar gemiye yüklendi : " + dataToGo);
+        }
+    }
+
+}
