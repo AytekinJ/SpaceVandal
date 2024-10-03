d6d186ab41727cdcd62120f79ed7f3f6b4c09994
AnyPerson1 <116653247+AnyPerson1@users.noreply.github.com>
2024-10-03 12:02:32 +0300

    planet display started

---
+++ projectVamparator/Assets/Software/Scripts/UI/PlanetDisplay/ScreenMove.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenMove : MonoBehaviour
+{
+    public void MoveUpwards()
+    {
+
+    }
+    public void MoveDownwards() 
+    {
+
+    }
+}
